faec0d66a26804bf114a2684d9dc46d9464c6eb8
HajimeNoOshi <markjosh791@gmail.com>
2025-10-26 02:42:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,18 +12,18 @@ function createHeart() {
 
     const path = document.createElementNS(svgNS, "path");
     path.setAttribute("d","M23.6,0c-2.9,0-5.4,1.7-6.6,4.1C15.8,1.7,13.3,0,10.4,0C4.7,0,0,4.7,0,10.4c0,11.5,16,19.2,16,19.2s16-7.7,16-19.2C32,4.7,27.3,0,23.6,0z");
-    path.setAttribute("fill","none");
-    path.setAttribute("stroke-width","2");
+    path.setAttribute("none","none");
+    path.setAttribute("stroke-width","10");
     path.setAttribute("stroke-linecap","round");
     path.setAttribute("stroke-linejoin","round");
 
     heart.appendChild(path);
     container.appendChild(heart);
 
-    setTimeout(() => heart.remove(), 2000);
+    setTimeout(() => heart.remove(), 50000);
 
-    index = (index + 1) % pattern.length;
+    index = (index + 2) % pattern.length;
 }
 
 // Spawn a heart every 0.5s
-setInterval(createHeart, 500);
+setInterval(createHeart, 5000);
